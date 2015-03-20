0896e6fee14d10c05a3bf0d305ac1d8af715da49
David Tschida <dmtschida1@gmail.com>
2015-03-20 12:39:10 -0400

    Adding models and pleasing the linter

---
+++ models/lab.js
@@ -0,0 +1,45 @@
+// app/models/user.js
+// load the things we need
+var mongoose = require('mongoose');
+
+var classifications = "itap cs ecn tech".split(" ");
+
+// define the schema for our user model
+var labSchema = mongoose.Schema({
+  name: String,
+  building: String,
+  room: String,
+
+  classification : { type: String, enum: classifications },
+  computers : [
+    {
+      os: { type : String, enum : "pc mac linux".split(" ") },
+      description : String,
+      total : Number,
+      available: Number
+    }
+  ],
+  printers : [
+    {
+      name: String, //color, bw
+      total : Number
+    }
+  ],
+  scanners : [
+    {
+      name: String,
+      total: Number,
+      available: Number
+    }
+  ],
+
+  limitations : String, //Open only to...
+  schedule : {
+    link : String
+  }
+});
+
+// methods ======================
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Lab', labSchema);
